9a823ccdde601ad47cbc368f44a23b2b8fec5397
SetiawanFadzli <setiawanFadzli@gmail.com>
2025-11-10 14:19:12 +0700

    Update form User

--- User.py
+++ User.py
@@ -20,6 +20,9 @@ class User(QMainWindow):
         self.tampilDataUser()
         self.btnSimpan.clicked.connect(self.simpanDataUser)
         self.tblUser.clicked.connect(self.tampilDataUserTerpilih)
+        self.btnUbah.clicked.connect(self.ubahDataUser)
+        self.btnHapus.clicked.connect(self.hapusDataUser)
+        self.tblUser.viewport().installEventFilter(self)
 
     def tampilPesan(self, pesan):
         msgbox=QMessageBox()
@@ -118,6 +121,80 @@ class User(QMainWindow):
         except:
             self.tampilPesan("Terjadi kesalahan pada saat memilih dan menampilkan data user!")
         
+    def ubahDataUser(self):
+        try:
+            id_user=self.editID.displayText()
+            nama_depan=self.editNamaDepan.displayText()
+            nama_tengah=self.editNamaTengah.displayText()
+            nama_belakang=self.editNamaBelakang.displayText()
+            tipe_user=self.cmbTipeUser.currentText()
+            username=self.editUsername.displayText()
+
+            if id_user!="" and nama_depan!="" and tipe_user!="" and username!="":
+                con=pymysql.connect(host="localhost",user="root",password="",database="lombokperpustakaan_db")
+                query="UPDATE tbl_user SET nama_depan=%s,nama_tengah=%s,nama_belakang=%s,tipe_user=%s,username=%s WHERE id_user=%s"
+                data=(nama_depan,nama_tengah,nama_belakang,tipe_user,username,id_user)
+                cursor=con.cursor()
+                cursor.execute(query,data)
+                con.commit()
+                con.close()
+                self.tampilPesan("Pembaruan Data User Berhasil")
+                self.tampilDataUser()
+            else:
+                self.tampilPesan("Data user tidak lengkap, silahkan lengkapi data terlebih dahulu")
+        except:
+            self.tampilPesan("Terjadi kesalahan pada saat memperbarui data")
+    def hapusDataUser(self):
+        try:
+            id_user=self.editID.displayText()
+            if id_user!="":
+                msgbox=self.tampilJendelaKonfirmasi("Apakah anda yakin ingin menghapus data ini?")
+                if msgbox==QMessageBox.Ok:
+                    con=pymysql.connect(host="localhost",user="root",password="",database="lombokperpustakaan_db")
+                    query="DELETE FROM tbl_user WHERE id_user=%s"
+                    data=(id_user,)
+                    cursor=con.cursor()
+                    cursor.execute(query,data)
+                    con.commit()
+                    con.close()
+                    self.tampilPesan("Data telah terhapus!")
+                    self.tampilDataUser()
+                    self.hapusText()
+            else:
+                self.tampilPesan('Data kosong, silahkan pilih data user yang ingin dihapus terlebih dahulu')
+        except:
+            self.tampilPesan('Terjadi kesalahan pada saat melakukan penghapusan data user')
+    def ubahPasswordUser(self,id_user,passwordBaru):
+        try:
+            if id_user!="" and passwordBaru!="":
+                con=pymysql.connect(host="localhost",user="root",password="",database="lombokperpustakaan_db")
+                query="UPDATE tbl_user SET password=md5(%s) WHERE id_user=%s"
+                data=(passwordBaru,id_user)
+                cursor=con.cursor()
+                cursor.execute(query,data)
+                con.commit()
+                con.close()
+                self.tampilPesan("Password telah diubah!")
+            else:
+                self.tampilPesan("Silahkan pilih user yang ingin diubah password terlebih dahulu!")
+        except:
+            self.tampilPesan("Terjadi kesalahan pada saat mengubah password!")
+    def eventFilter(self,source,event):
+        try:
+            if event.type()==QEvent.ContextMenu and source is self.tblUser.viewport():
+                menu=QMenu()
+                menu.addAction(QIcon('ICONS/change_password.png'),'Ubah Password')
+
+                if menu.exec_(event.globalPos()):
+                    items=self.tblUser.selectedItems()
+                    id_user=items[0].text()
+                    passwordBaru,ok=QInputDialog.getText(self,'Lombok Perpustakaan','Password Baru', QLineEdit.Password, '')
+
+                    if ok:
+                        self.ubahPasswordUser(id_user,passwordBaru)
+            return super().eventFilter(source,event)
+        except:
+            self.tampilPesan('Terjadi kesalahan pada saat membuat kontak menu')
 if __name__=="__main__":
     app=QApplication(sys.argv)
     form=User()
